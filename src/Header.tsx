a59edcc61847696762d63f089443209ca117a0b0
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-08-12 00:06:13 +0200

    displaying hamburger icon with styling

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,4 @@
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
 import './App.css';
@@ -16,6 +16,9 @@ const Header = () => {
           <li>Contact</li>
           <li>Work with us</li>
         </ul>
+        <HamburgerIcon>
+          <FontAwesomeIcon icon={faBars} style={{ color: 'white', fontSize: '2.2rem', cursor: 'pointer' }} />
+        </HamburgerIcon>
       </nav>
     </HeaderContainer>
   );
@@ -33,6 +36,17 @@ const HouseIcon = styled.div`
   color: white;
   margin-left: 25px;
   font-size: 2.2rem;
+`;
+
+
+const HamburgerIcon = styled.div`
+  display: none;
+
+  @media (max-width: 640px) {
+    display: block;
+    padding: 0 2rem;
+    align-items: center;
+  }
 `;
 
 export default Header;
