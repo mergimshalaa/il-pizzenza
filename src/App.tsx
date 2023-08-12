5dd915c7dcf442561325ebf6d330ef92ed956da8
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-08-13 00:03:46 +0200

    firstpage component into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import Header from './Header';
-
 import FirstPage from './HomePage';
-import { Footer } from './Footer';
-import ProductPage from './ProductPage';
+
 
 function App() {
 
   return (
     <>
     <Header />
+    <FirstPage />
     </>
   )
 }
