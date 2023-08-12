d94d3f3fc5def8f836b13372fc55e6e8d3b5a231
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-08-12 21:26:12 +0200

    add wrapper with styling, displaying bgImg

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,16 +1,35 @@
+import styled from "styled-components";
 import logo from "./img/logo1.png";
-
+import backgroundImage from "./img/ManarolaItaly.png";
 
 const FirstPage = () => {
   return (
-    <>
-      <img src={logo} alt="Logotyp" />
-      <p>
-        Your local pasta bar in Manarola. Homemade pasta & pizza in the classic
-        Italian way with small dishes made from fresh ingredients.
-      </p>
-    </>
+    <HomePageWrapper>
+      <div>
+        <LogoImage src={logo} alt="Logotyp" />
+        <p>
+          Your local pasta bar in Manarola. Homemade pasta & pizza in the
+          classic Italian way with small dishes made from fresh ingredients.
+        </p>
+      </div>
+    </HomePageWrapper>
   );
 };
+
+const HomePageWrapper = styled.div`
+  background-image: url(${backgroundImage});
+  background-attachment: fixed;
+  background-position: center;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LogoImage = styled.img`
+  width: 500px;
+  height: 360px;
+`;
 
 export default FirstPage;
