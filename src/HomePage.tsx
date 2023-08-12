b6d1fa4b841019cd2d5bea82b8735171f20820dd
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-08-12 20:35:34 +0200

    template homepage displaying logo& presentation txt

---
+++ src/HomePage.tsx
@@ -0,0 +1,16 @@
+import logo from "./img/logo1.png";
+
+
+const FirstPage = () => {
+  return (
+    <>
+      <img src={logo} alt="Logotyp" />
+      <p>
+        Your local pasta bar in Manarola. Homemade pasta & pizza in the classic
+        Italian way with small dishes made from fresh ingredients.
+      </p>
+    </>
+  );
+};
+
+export default FirstPage;
