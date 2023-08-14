a74fd78e43785822be9b4b36c7bc03487bf17feb
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-08-15 00:23:45 +0200

    footer incl. openinghours, contact & adress (WIP)

---
+++ src/Footer.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export function Footer() {
+  return (
+    <footer>
+      
+      <div>
+        <div>
+          <h3>OPENING HOURS</h3>
+        </div>
+        <div>
+          <ul>
+            <li>Monday</li>
+            <li>Tuesday</li>
+            <li>Wednesday</li>
+            <li>Thursday</li>
+            <li>Friday</li>
+            <li>Saturday </li>
+            <li>Sunday</li>
+          </ul>
+          <ul>
+            <li>5pm - 10pm</li>
+            <li>5pm - 10pm</li>
+            <li>5pm - 10pm</li>
+            <li>5pm - 10pm</li>
+            <li>6pm - 11pm</li>
+            <li>7pm - 11pm </li>
+            <li>CLOSED</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h3>CONTACT</h3>
+        </div>
+        <p>ilpizzenza@manarola.com</p>
+        <p>TEL: 0000-00000</p>
+      </div>
+
+      <div>
+        <div>
+          <h3>ADRESS</h3>
+        </div>
+        <p>VIA Renato Birolli 149 19017 Manarola, Riomaggiore SP ITALY</p>
+      </div>
+
+    </footer>
+  );
+}
